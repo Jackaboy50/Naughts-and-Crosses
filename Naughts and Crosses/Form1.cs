a1aed55c460439cf1aeff165d2d646e48960c459
Jackaboy50 <119588287+Jackaboy50@users.noreply.github.com>
2023-09-18 12:15:03 +0100

    added board grid

--- Naughts and Crosses/Form1.cs
+++ Naughts and Crosses/Form1.cs
@@ -24,6 +24,35 @@ namespace Naughts_and_Crosses
                     gameBoard[x, y] = new BoardTile(x * 200, y * 200, this);
                 }
             }
+
+            int pointOffset = 200;
+            for (int i = 0; i < 4; i++)
+            {
+                Button gridButton = new Button();
+                Size size = new Size();
+                Point point = new Point();
+                
+                if(i < 2)
+                {
+                    size = new Size(600, 5);
+                    point = new Point(0, pointOffset);
+                }
+                else
+                {
+                    size = new Size(5, 600);
+                    point = new Point(pointOffset - 400, 0);
+                }
+
+                gridButton.Size = size;
+                gridButton.Location = point;
+                gridButton.FlatStyle = FlatStyle.Flat;
+                gridButton.FlatAppearance.BorderSize = 0;
+                gridButton.BackColor = Color.Black;
+                gridButton.Enabled = false;
+                Controls.Add(gridButton);
+                Controls[Controls.Count - 1].BringToFront();
+                pointOffset += 200;
+            }
         }
 
         private void SetForm()
